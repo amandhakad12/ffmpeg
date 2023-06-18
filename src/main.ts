f61710e39f0e782298122d5a2790438050cd5674
Aman Dhakad <amandhakad2002@gmail.com>
2023-06-18 14:45:52 +0530

    added express middlewares - json, static, cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use("/public", express.static("public"));
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("listening");
